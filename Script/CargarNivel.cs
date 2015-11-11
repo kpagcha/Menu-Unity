317b5e717aee86ba30e134407fe59c82febd1dc5
kpagcha <4d.garcia6@gmail.com>
2015-11-11 20:58:04 +0100

    Primer commit

---
+++ Script/CargarNivel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Fungus;
+
+public class CargarNivel : MonoBehaviour
+{
+	public string escena;
+	
+	void CargarEscena ()
+	{
+		Flowchart fc = GameObject.FindObjectOfType<Flowchart> ();
+		escena = fc.GetStringVariable ("Escena");;
+		Application.LoadLevel (escena);
+	}
+}
